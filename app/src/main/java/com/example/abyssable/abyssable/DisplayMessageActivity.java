e7a22cfcc404d4a7e549e6cc8fa0aedbcc319930
Dan Patrascu <dan_patrashcu@yahoo.com>
2015-03-20 23:31:02 +0200

    added sound recorder and photo capture tests

--- app/src/main/java/com/example/abyssable/abyssable/DisplayMessageActivity.java
+++ app/src/main/java/com/example/abyssable/abyssable/DisplayMessageActivity.java
@@ -1,14 +1,34 @@
 package com.example.abyssable.abyssable;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
+
+    private static final int REQUEST_TAKE_PHOTO = 0x7712;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile = null;
+    private Button start,stop,play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +36,78 @@ public class DisplayMessageActivity extends ActionBarActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(StartUp.EXTRA_MESSAGE);
-
+/*
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
 
         // Set the text view as the activity layout
-        setContentView(textView);
+        setContentView(textView);*/
+
+        setContentView(R.layout.activity_display_message);
+        start = (Button)findViewById(R.id.button1);
+        stop = (Button)findViewById(R.id.button2);
+        play = (Button)findViewById(R.id.button3);
+
+        stop.setEnabled(false);
+        play.setEnabled(false);
+        outputFile = Environment.getExternalStorageDirectory().
+                getAbsolutePath() + "/myrecording.3gp";;
+
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+
+    }
+    public void start(View view){
+        try {
+            myAudioRecorder.prepare();
+            myAudioRecorder.start();
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        start.setEnabled(false);
+        stop.setEnabled(true);
+        Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+
+        dispatchTakePictureIntent();
+
     }
 
+    public void stop(View view){
+        stop();
+    }
+
+    private void stop()    {
+        myAudioRecorder.stop();
+        myAudioRecorder.release();
+        myAudioRecorder  = null;
+        stop.setEnabled(false);
+        play.setEnabled(true);
+        Toast.makeText(getApplicationContext(), "Audio recorded successfully",
+                Toast.LENGTH_LONG).show();
+
+
+        //get only last5 sec of recording
+
+    }
+
+    public void play(View view) throws IllegalArgumentException,
+            SecurityException, IllegalStateException, IOException{
+
+        MediaPlayer m = new MediaPlayer();
+        m.setDataSource(outputFile);
+        m.prepare();
+        m.start();
+        Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,5 +129,57 @@ public class DisplayMessageActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                       // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                //save
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0x7712)
+            stop();
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
